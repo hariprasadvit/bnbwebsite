fb970b6f58f1867a607ac7d32926e5e39099cd4a
Kakashi Hatake <brinton@cartoonmango.com>
2025-06-06 15:32:21 +0530

    [Fixed] Header Responsive

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "274e99c8d9129824"; }
+/******/ 		__webpack_require__.h = function() { return "b6f0a4a4692bd45a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
