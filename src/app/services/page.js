80227eb94eff9231a291f942889297dcb31cb5e6
Karthik Ramasamy <karthik@cartoonmango.com>
2025-07-16 15:26:16 +0530

    fix for netlify deploy

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,28 +1,16 @@
 import dynamic from "next/dynamic";
 import qs from "qs";
-import { serviceListingBannerData } from "@/components/data";
 import Header from "@/components/Common/Header";
-import { unstable_noStore } from "next/cache";
+import { unstable_noStore as noStore } from "next/cache";
 import { getStrapiURL } from "@/lib/utils";
 import { fetchAPI } from "@/lib/fetch-api";
 import { notFound } from "next/navigation";
 import BlockRenderer from "@/components/Services/BlockRenderer";
 
 const Banner = dynamic(() => import("@/components/Home/Banner"));
-const OurService = dynamic(() => import("@/components/Services/OurService"));
-const OurWorks = dynamic(() => import("@/components/Home/OurWorks"));
-const NumberSection = dynamic(() => import("@/components/Home/NumberSection"));
-const OurClients = dynamic(() => import("@/components/Home/OurClients"));
-const Testimonials = dynamic(() => import("@/components/Home/Testimonials"));
-const InsightsAndBlog = dynamic(() =>
-  import("@/components/Home/InsightsAndBlog")
-);
-const IndustryWeServe = dynamic(() =>
-  import("@/components/Services/IndustryWeServe")
-);
 
 async function loader() {
-  unstable_noStore();
+  noStore();
   const BASE_URL = getStrapiURL();
   const path = "/api/service-listing";
 
