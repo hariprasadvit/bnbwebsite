03ad4e82bc360bae1ad5bba0d9a602982d84cfb6
Karthik Ramasamy <karthik@cartoonmango.com>
2025-07-19 15:46:16 +0530

    Service detail page setup

---
+++ src/app/services/[slug]/page.js
@@ -0,0 +1,73 @@
+import qs from "qs";
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import Header from "@/components/Common/Header";
+import { getStrapiURL } from "@/lib/utils";
+import { unstable_noStore as noStore } from "next/cache";
+import { notFound } from "next/navigation";
+
+import BlockRendererDetail from "@/components/Services-old/BlockRendererDetail";
+import { fetchAPI } from "@/lib/fetch-api";
+
+const Banner = dynamic(() => import("@/components/Home/Banner"));
+
+async function loader({ slug }) {
+  if (!slug) {
+    notFound();
+  }
+  noStore();
+  const BASE_URL = getStrapiURL();
+  const path = `/api/service-details/${slug}`;
+
+  const query = qs.stringify(
+    {
+      pLevel: "5",
+    },
+    { encodeValuesOnly: true }
+  );
+
+  const url = new URL(path + "?" + query, BASE_URL);
+  const data = await fetchAPI(url.href, {
+    method: "GET",
+  });
+  if (!data.data) notFound();
+  const blocks = data?.data?.common || [];
+  const pageContent = data?.data || {};
+
+  return { blocks, pageContent };
+}
+
+export default async function ServiceDetails({ params }) {
+  const blockData = await loader({
+    slug: params?.slug,
+  });
+  return (
+    <div>
+      <Head>
+        <title>B&B</title>
+      </Head>
+      <div style={{ width: "100%" }}>
+        <Banner
+          data={blockData?.pageContent}
+          hideBorder
+          contactUs={blockData?.pageContent?.button}
+          highlightFirst
+          descriptionMaxWidth={"828px"}
+          headingMarginBottom={30}
+        />
+        {/* <MvpStartUpCard data={mvpCardContent} />
+        <PlatformCardContainer platformCards={servicePlatformCards} />
+        <ProductDevelopment
+          title={
+            " We don’t just build MVPs for startups,we create clarity, ignitemomentum, and lay the foundation for scalable success."
+          }
+          productDevelopmentData={productDevelopmentData}
+        />
+        <DataDriven titleMaxWidth={"305px"} titleMarginBottom={"70px"} />
+        <Testimonials />
+        <InsightsAndBlog /> */}
+        <BlockRendererDetail blocks={blockData?.pageContent?.dynamic_section} />
+      </div>
+    </div>
+  );
+}
