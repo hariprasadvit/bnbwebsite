16d2d81994f0be239a4b3de0a71bce80808809f7
Karthik Ramasamy <karthik@cartoonmango.com>
2025-07-29 19:37:03 +0530

    ui fix'

--- src/components/Home/HireWithForm/index.js
+++ src/components/Home/HireWithForm/index.js
@@ -173,7 +173,7 @@ export default function HireWithForm({ pageTitle }) {
             </div>
             <div className={styles.inputWrapper}>
               <input
-                placeholder="Mobile Number"
+                placeholder="Mobile Number*"
                 type="number"
                 onChange={(e) => onChange(e.target.value, "phone_number")}
                 onKeyDown={(e) => {
