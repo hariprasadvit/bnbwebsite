c54744af7dd77f8beec4cf797e747ba5fe7215cf
Karthik Ramasamy <karthik@cartoonmango.com>
2025-07-30 14:04:11 +0530

    case study optimisation

--- src/components/DigitalProducts/BannerImage/index.js
+++ src/components/DigitalProducts/BannerImage/index.js
@@ -5,7 +5,14 @@ import styles from "../../../styles/page.module.scss";
 const BannerImage = ({ url = "" }) => {
   return (
     <section className={styles.bannerImage}>
-      <Image src={url} alt="Banner Image" width={0} height={0} />
+      <Image
+        src={url}
+        alt="Banner Image"
+        width={1470}
+        height={916}
+        loading="lazy"
+        fetchPriority="high"
+      />
       {/* <div className={styles.bannerImageBottomContainer}>
         <OverViewCard overviewData={digitalProductoverviewData} />
         <DataDriven showDataDrivenImg={false} />
