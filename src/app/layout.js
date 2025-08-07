6113672ae8181620fa3c06dfe6c19f6a29a8950f
Karthik Ramasamy <karthik@cartoonmango.com>
2025-08-07 14:35:14 +0530

    Added - gtag code

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.scss";
 import Footer from "@/components/Common/Footer";
 import HeaderWrapper from "@/components/Common/Header/HeaderWrapper";
 import PageLoaderWrapper from "@/components/Common/FullPageLoader/PageLoaderWrapper";
+import Script from "next/script";
 
 const titillium = Titillium_Web({
   subsets: ["latin"],
@@ -13,6 +14,21 @@ const titillium = Titillium_Web({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={titillium.variable}>
+      <head>
+        {/* Google Tag Manager Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RS4FR1L7EL"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RS4FR1L7EL');
+          `}
+        </Script>
+      </head>
       <body>
         <PageLoaderWrapper>
           <HeaderWrapper />
