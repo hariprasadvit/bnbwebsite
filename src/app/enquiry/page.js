e7d275e8e2ebb5df029015006e073f6091ab3607
Karthik Ramasamy <karthik@cartoonmango.com>
2025-08-21 16:41:56 +0530

    Enquiry page full code

--- src/app/enquiry/page.js
+++ src/app/enquiry/page.js
@@ -1,13 +1,45 @@
+import qs from "qs";
+import { fetchAPI } from "@/lib/fetch-api";
+
 import FAQ from "@/components/Common/FAQ";
 import FooterForm from "@/components/ContactUs/HireForm";
 import AIpoweredSection from "@/components/Enquiry/AIpoweredSection";
+import BlockRenderer from "@/components/Enquiry/BlockRenderer";
 import CardSection from "@/components/Enquiry/CardSection";
 import ChatSection from "@/components/Enquiry/ChatSection";
 import WhyChoose from "@/components/Enquiry/WhyChoose";
 import Banner from "@/components/Projects/Banner";
 import React from "react";
+import { unstable_noStore as noStore } from "next/cache";
+import { getStrapiURL } from "@/lib/utils";
 
-export default function Enquiry() {
+async function loader() {
+  noStore();
+  const BASE_URL = getStrapiURL();
+  const path = "/api/landing-page";
+
+  const query = qs.stringify(
+    {
+      pLevel: "5",
+    },
+    { encodeValuesOnly: true }
+  );
+
+  const url = new URL(path + "?" + query, BASE_URL);
+  const data = await fetchAPI(url.href, {
+    method: "GET",
+  });
+
+  if (!data.data) notFound();
+
+  const blocks = data?.data?.common || [];
+  const pageContent = data?.data || {};
+
+  return { blocks, pageContent };
+}
+
+export default async function Enquiry() {
+  const { blocks } = await loader();
   return (
     <div
       style={{
@@ -28,6 +60,7 @@ export default function Enquiry() {
       <ChatSection />
       <AIpoweredSection />
       <WhyChoose />
+      <BlockRenderer blocks={blocks} />
       <FAQ />
       <FooterForm />
     </div>
--- src/components/Enquiry/WhyChoose/index.js
+++ src/components/Enquiry/WhyChoose/index.js
@@ -1,36 +1,11 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import BrainCircuit from "../../../../public/brain-circuit.svg";
-import Image from "next/image";
 export default function WhyChoose() {
-  const data = [
-    {
-      title: "Rapid MVP Development",
-      description: "2 features per week with ai assisted Rapid development",
-      image: BrainCircuit,
-      alt: "brain-circuit",
-    },
-    {
-      title: "AI Powered Development",
-      description: "2 features per week with ai assisted Rapid development",
-      image: BrainCircuit,
-      alt: "brain-circuit",
-    },
-    {
-      title: "AI Powered Development",
-      description: "2 features per week with ai assisted Rapid development",
-      image: BrainCircuit,
-      alt: "brain-circuit",
-    },
-  ];
   return (
     <div className={styles.WhyChooseSection}>
       <div className={styles.WhyChooseContainer}>
         <div className={styles.title}>
-          <h2>
-            SHIP MAP’S FASTER WITH
-            <span>AI + HUMAN EXPERTISE</span>
-          </h2>
+          <h2>Why choose us for B2B Development</h2>
           <p>
             We help businesses turn bold ideas into functional, user-validated
             MVPs through a thoughtful, data-driven strategy. From initial
@@ -39,15 +14,32 @@ export default function WhyChoose() {
           </p>
         </div>
         <div className={styles.CardContainer}>
-          {data.map((item, index) => (
-            <div className={styles.cardItem} key={index}>
-              <div className={styles.imageContainer}>
-                <Image src={item.image} alt={item.alt} width={35} height={35} />
-              </div>
-              <strong>{item.title}</strong>
-              <p>{item.description}</p>
+          <div className={styles.left}>
+            <ul>
+              <li>Make data driven decisions with real</li>
+              <li>Make data driven decisions with real and som</li>
+              <li>Make data driven decisions with real</li>
+              <li>Make data driven decisions</li>
+            </ul>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.card}>
+              <strong>30+</strong>
+              <span>SaaS Projects</span>
             </div>
-          ))}
+            <div className={styles.card}>
+              <strong>30+</strong>
+              <span>SaaS Projects</span>
+            </div>
+            <div className={styles.card}>
+              <strong>30+</strong>
+              <span>SaaS Projects</span>
+            </div>
+            <div className={styles.card}>
+              <strong>30+</strong>
+              <span>SaaS Projects</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
